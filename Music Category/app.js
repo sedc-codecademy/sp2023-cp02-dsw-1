b67bffc506e54af833f72538c9582e7e3e112bb7
NikolaStojkovski <nikola.stojkovski2003@gmail.com>
2023-06-08 10:52:57 +0200

    Music Category

--- Music Category/app.js
+++ Music Category/app.js
@@ -1,7 +1,10 @@
 const mainBody = document.querySelector(".main");
 const guitars = document.querySelector(".guitars");
 const cardSection = document.querySelector(".card-section");
-filteredArray = [];
+const bassGuitars = document.querySelector(".bass-guitars");
+const keys = document.querySelector(".keys");
+const drums = document.querySelector(".drums");
+const microphones = document.querySelector(".microphones");
 
 const displayData = (dummyData) => {
   return (cardSection.innerHTML = dummyData
@@ -21,9 +24,9 @@ const displayData = (dummyData) => {
     .join(""));
 };
 
-const guitarsFilter = (filteredArray, dummyData) => {
+const categoryFilter = (filteredArray, dummyData, category) => {
   for (let product of dummyData) {
-    if (product.category === 1) {
+    if (product.category === category) {
       filteredArray.push(product);
     }
   }
@@ -46,22 +49,72 @@ const displayCategoryData = (dummyData) => {
     .join(""));
 };
 
-guitars.addEventListener("click", () => {
-  cardSection.innerHTML = "";
-  displayCategoryData(filteredArray);
-  guitars.classList.add("clickedButton");
-});
+const guitarButton = (productsData) => {
+  const filteredArray = [];
+  categoryFilter(filteredArray, productsData, 1);
+  guitars.addEventListener("click", () => {
+    cardSection.innerHTML = "";
+    displayCategoryData(filteredArray);
+    guitars.classList.add("clickedButton");
+  });
+};
+
+const bassGuitarButton = (productsData) => {
+  const filteredArray = [];
+  categoryFilter(filteredArray, productsData, 2);
+  bassGuitars.addEventListener("click", () => {
+    cardSection.innerHTML = "";
+    displayCategoryData(filteredArray);
+    bassGuitars.classList.add("clickedButton");
+  });
+};
+
+const keysButton = (productsData) => {
+  const filteredArray = [];
+  categoryFilter(filteredArray, productsData, 3);
+  keys.addEventListener("click", () => {
+    cardSection.innerHTML = "";
+    displayCategoryData(filteredArray);
+    keys.classList.add("clickedButton");
+  });
+};
+
+const drumsButton = (productsData) => {
+  const filteredArray = [];
+  categoryFilter(filteredArray, productsData, 4);
+  drums.addEventListener("click", () => {
+    cardSection.innerHTML = "";
+    displayCategoryData(filteredArray);
+    drums.classList.add("clickedButton");
+  });
+};
+
+const microphonesButton = (productsData) => {
+  const filteredArray = [];
+  categoryFilter(filteredArray, productsData, 5);
+  microphones.addEventListener("click", () => {
+    cardSection.innerHTML = "";
+    displayCategoryData(filteredArray);
+    microphones.classList.add("clickedButton");
+  });
+};
 
 // Fetching data from local JSON file
 const fetchLocalData = async () => {
   try {
     const res = await fetch("/products.json");
     const data = await res.json();
-    guitarsFilter(filteredArray, data);
+    microphonesButton(data);
+    drumsButton(data);
+    keysButton(data);
+    bassGuitarButton(data);
+    guitarButton(data);
     displayData(data);
     return data;
   } catch (error) {
     throw new Error(error);
   }
 };
-console.log(fetchLocalData());
+fetchLocalData();
+
+categoryFilter(filteredArray, data);
