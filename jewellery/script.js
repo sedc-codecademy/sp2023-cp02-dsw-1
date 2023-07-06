05f6f7bec81643faaa8a4be800e7af0667fe91ba
Marko Ingjikj <ingjikjmarko2003@gmail.com>
2023-07-06 12:29:30 +0200

    Added nav and footer to all htmls

--- jewellery/script.js
+++ jewellery/script.js
@@ -1,8 +1,40 @@
+const searchBtn = document.querySelector(".search-button");
+const searchInput = document.querySelector(".search-bar");
+const searchBtnPhone = document.querySelector(".search-btn-phone");
+const searchInputPhone = document.querySelector(".search-bar-phone");
 const menuButton = document.querySelector(".menu");
 const menu = document.querySelector(".categories-and-deals-list-phone");
 const categoriesButton = document.querySelector(".categories-phone-link");
 const showSearchButton = document.querySelector(".search-button-phone");
 const searchDisplay = document.querySelector(".search-display-phone");
+
+searchInputPhone.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    const url = searchInput.value.toLowerCase();
+    window.open(`../${url}/index.html`);
+  }
+});
+
+searchBtnPhone.addEventListener("click", () => {
+  const url = searchInputPhone.value;
+  window.open(`../${url}/index.html`);
+});
+
+searchInput.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    const url = searchInput.value.toLowerCase();
+    console.log(url);
+
+    window.open(`../${url}/index.html`);
+  }
+});
+
+searchBtn.addEventListener("click", () => {
+  const url = searchInput.value;
+  console.log(url);
+
+  window.open(`../${url}/index.html`);
+});
 
 const menuDisplay = () => {
   searchDisplay.style.display = "none";
@@ -24,8 +56,7 @@ const menuDisplay = () => {
  </div>
 </li>
 <li class="phone-menu-item middle-item"><a href="../deals/index.html">Deals</a></li>
-<li class="phone-menu-item middle-item"><a href="../contact/index.html">Contact</a></li>
-<li class="phone-menu-item"><a href="../index.html">SEDC Home</a></li>
+<li class="phone-menu-item"><a href="../contact/index.html">Contact</a></li>
 </ul>`;
 };
 
@@ -47,45 +78,38 @@ const collapseMenu = () => {
     </ul>
   </div>
 </li>
-<li class="phone-menu-item middle-item"><a href="#">Deals</a></li>
-<li class="phone-menu-item middle-item"><a href="#">Contact</a></li>
-<li class="phone-menu-item"><a href="../index.html">SEDC Home</a></li>
+<li class="phone-menu-item middle-item"><a href="../deals/index.html">Deals</a></li>
+<li class="phone-menu-item"><a href="../contact/index.html">Contact</a></li>
 </ul>`;
 };
 
 const showCategories = () => {
-  menu.innerHTML = `
-  <svg class="back-arrow" stroke="currentColor" fill="currentColor" stroke-width="0" 
-  viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-  <path d="M872 474H286.9l350.2-304c5.6-4.9 2.2-14-5.2-14h-88.5c-3.9 0-7.6 1.4-10.5 3.9L155 
-  487.8a31.96 31.96 0 0 0 0 48.3L535.1 866c1.5 1.3 3.3 2 5.2 2h91.5c7.4 0 10.8-9.2 5.2-14L286.9 
-  550H872c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z" ></path></svg>
-  <li class="dropdown-li-phone border-dropdown"><a href="../clothes/index.html">Clothes</a></li>
-  <li class="dropdown-li-phone border-dropdown"><a href="../shoes/index.html">Shoes</a></li>
-  <li class="dropdown-li-phone border-dropdown"><a href="./index.html">Jewellery</a></li>
+  menu.innerHTML = `  
+  <svg class="back-arrow" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M872 474H286.9l350.2-304c5.6-4.9 2.2-14-5.2-14h-88.5c-3.9 0-7.6 1.4-10.5 3.9L155 487.8a31.96 31.96 0 0 0 0 48.3L535.1 866c1.5 1.3 3.3 2 5.2 2h91.5c7.4 0 10.8-9.2 5.2-14L286.9 550H872c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z" ></path></svg>
+  <li class="dropdown-li-phone border-dropdown"><a href="#">Clothes</a></li>
+  <li class="dropdown-li-phone border-dropdown"><a href="#">Shoes</a></li>
+  <li class="dropdown-li-phone border-dropdown"><a href="../jewellery/index.html">Jewellery</a></li>
   <li class="dropdown-li-phone border-dropdown"><a href="../electronics/index.html">Electronics</a></li>
   <li class="dropdown-li-phone border-dropdown"><a href="../music/index.html">Music</a></li>
-  <li class="dropdown-li-phone"><a href="../furniture/index.html">Furniture</a></li>`;
+  <li class="dropdown-li-phone"><a href="#">Furniture</a></li>`;
 
+  const backArrow = document.querySelector(".back-arrow");
 
-const backArrow = document.querySelector(".back-arrow");
-
-backArrow.addEventListener("click", () => {
-  menu.innerHTML = `
-  <li class="phone-menu-item">
- <div class="categories-menu-phone">
-   <ul class="categories-ul-phone">
-     <li>
-       <div class="categories-phone-link" onclick='showCategories()'>Categories</div>
-     </li>
-   </ul>
- </div>
-</li>
-<li class="phone-menu-item middle-item"><a href="#">Deals</a></li>
-<li class="phone-menu-item middle-item"><a href="#">Contact</a></li>
-<li class="phone-menu-item"><a href="../index.html">SEDC Home</a></li>
-</ul>`;
-});
+  backArrow.addEventListener("click", () => {
+    menu.innerHTML = `
+    <li class="phone-menu-item">
+   <div class="categories-menu-phone">
+     <ul class="categories-ul-phone">
+       <li>
+         <div class="categories-phone-link" onclick='showCategories()'>Categories</div>
+       </li>
+     </ul>
+   </div>
+  </li>
+  <li class="phone-menu-item middle-item"><a href="../deals/index.html">Deals</a></li>
+  <li class="phone-menu-item"><a href="../contact/index.html">Contact</a></li>
+  </ul>`;
+  });
 };
 
 const showSearch = () => {
@@ -97,7 +121,6 @@ const showSearch = () => {
     searchDisplay.style.display = "block";
   }
 };
-
 
 //MENU SECTION
 const filteredArray = [];
@@ -287,11 +310,9 @@ const allNecklacesBtn = (productsData) => {
 const allProducts = document.querySelector(".allProducts");
 
 const printAllProducts = (alldata, dummyData) => {
-  for (let product of dummyData) 
-    {
-      alldata.push(product);
-    }
-  
+  for (let product of dummyData) {
+    alldata.push(product);
+  }
 };
 
 const allProductsButton = (productsData) => {
@@ -309,7 +330,7 @@ const fetchLocalData = async () => {
   try {
     const res = await fetch("products.json");
     const data = await res.json();
-  
+
     displayData(data);
 
     allEarringsBtn(data);
